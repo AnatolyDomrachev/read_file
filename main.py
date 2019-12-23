e7d19bc5354898d12cb96680adfce3e2c59efc3a
Ваше Имя <you@example.com>
2019-12-23 11:43:18 +0300

    turing-2

--- main.py
+++ main.py
@@ -4,160 +4,92 @@ def turing(data):
     state = 'q1'
 
     while(True):
-        if state == "q1" and lenta[pos] == "0":
-            state = "q1"
-            lenta[pos] = "0"
-            pos += 1
-        if state == "q1" and lenta[pos] == "1":
-            state = "q1"
-            lenta[pos] = "1"
-            pos += 1
-        if state == "q1" and lenta[pos] == "2":
-            state = "q1"
-            lenta[pos] = "2"
+        if state == "q1" and lenta[pos] in [" ", "0", "1", "2"]:
             pos += 1
         if state == "q1" and lenta[pos] == "+":
             state = "q2"
-            lenta[pos] = "+"
             pos += 1
         if state == "q1" and lenta[pos] == "-":
             state = "q3"
-            lenta[pos] = "-"
-            pos += 1
-        if state == "q1" and lenta[pos] == " ":
-            state = "q1"
-            lenta[pos] = " "
             pos += 1
         if state == "q2" and lenta[pos] == "0":
             state = "q20"
-            lenta[pos] = "0"
             pos -= 1
         if state == "q2" and lenta[pos] == "1":
             state = "q21"
-            lenta[pos] = "1"
             pos -= 1
         if state == "q2" and lenta[pos] == "2":
             state = "q22"
-            lenta[pos] = "2"
             pos -= 1
         if state == "q3" and lenta[pos] == "0":
             state = "q20"
-            lenta[pos] = "0"
             pos -= 1
         if state == "q3" and lenta[pos] == "1":
             state = "q31"
-            lenta[pos] = "1"
             pos -= 1
         if state == "q3" and lenta[pos] == "2":
             state = "q32"
-            lenta[pos] = "2"
             pos -= 1
-        if state == "q20" and lenta[pos] == "0":
+        
+        if state == "q20" and lenta[pos] in ["0", "1", "2"]:
             state = "q60"
-            lenta[pos] = "0"
-        if state == "q20" and lenta[pos] == "1":
-            state = "q60"
-            lenta[pos] = "1"
-        if state == "q20" and lenta[pos] == "2":
-            state = "q60"
-            lenta[pos] = "2"
-        if state == "q20" and lenta[pos] == "+":
-            state = "q20"
-            lenta[pos] = "+"
+
+        if state in ["q20", "q21", "q22"] and lenta[pos] in ["+", "-"]:
             pos -= 1
-        if state == "q20" and lenta[pos] == "-":
-            state = "q20"
-            lenta[pos] = "-"
-            pos -= 1
-        if state == "q21" and lenta[pos] == "0":
+        
+        if state == "q21" and lenta[pos] in ["0", "1"]:
             state = "q60"
-            lenta[pos] = "1"
-        if state == "q21" and lenta[pos] == "1":
-            state = "q60"
-            lenta[pos] = "2"
+            lenta[pos] = str(int(lenta[pos])+1)
+
         if state == "q21" and lenta[pos] == "2":
             state = "q40"
             lenta[pos] = "0"
             pos -= 1
-        if state == "q21" and lenta[pos] == "+":
-            state = "q21"
-            lenta[pos] = "+"
-            pos -= 1
-        if state == "q21" and lenta[pos] == "-":
-            state = "q21"
-            lenta[pos] = "-"
-            pos -= 1
+
         if state == "q22" and lenta[pos] == "0":
             state = "q60"
             lenta[pos] = "2"
-        if state == "q22" and lenta[pos] == "1":
+
+        if state == "q22" and lenta[pos] in ["1", "2"]:
             state = "q40"
-            lenta[pos] = "0"
+            lenta[pos] = str(int(lenta[pos])-1)
             pos -= 1
-        if state == "q22" and lenta[pos] == "2":
-            state = "q40"
-            lenta[pos] = "1"
-            pos -= 1
-        if state == "q22" and lenta[pos] == "+":
-            state = "q22"
-            lenta[pos] = "+"
-            pos -= 1
-        if state == "q22" and lenta[pos] == "-":
-            state = "q22"
-            lenta[pos] = "-"
-            pos -= 1
+        
         if state == "q31" and lenta[pos] == "0":
             state = "q50"
             lenta[pos] = "2"
             pos -= 1
-        if state == "q31" and lenta[pos] == "1":
+
+        if state == "q31" and lenta[pos] in ["1", "2"]:
             state = "q60"
-            lenta[pos] = "0"
-        if state == "q31" and lenta[pos] == "2":
-            state = "q60"
-            lenta[pos] = "1"
-        if state == "q31" and lenta[pos] == "+":
-            state = "q31"
-            lenta[pos] = "+"
+            lenta[pos] = str(int(lenta[pos])-1)
+
+        if state == "q31" and lenta[pos] in ["+", "-"]:
             pos -= 1
-        if state == "q31" and lenta[pos] == "-":
-            state = "q31"
-            lenta[pos] = "-"
-            pos -= 1
+
         if state == "q32" and lenta[pos] == "0":
             state = "q50"
-            lenta[pos] = "1"
+            lenta[pos] = str(int(lenta[pos])+1)
             pos -= 1
-        if state == "q32" and lenta[pos] == "1":
-            state = "q50"
-            lenta[pos] = "2"
-            pos -= 1
+
         if state == "q32" and lenta[pos] == "2":
             state = "q60"
             lenta[pos] = "0"
-        if state == "q32" and lenta[pos] == "+":
-            state = "q32"
-            lenta[pos] = "+"
+
+        if state == "q32" and lenta[pos] in ["+", "-"]:
             pos -= 1
-        if state == "q32" and lenta[pos] == "-":
-            state = "q32"
-            lenta[pos] = "-"
-            pos -= 1
+
         if state == "q40" and lenta[pos] == "0":
             state = "q60"
-            lenta[pos] = "1"
-        if state == "q40" and lenta[pos] == "1":
-            state = "q60"
-            lenta[pos] = "2"
+            lenta[pos] = str(int(lenta[pos])+1)
+
         if state == "q40" and lenta[pos] == "2":
-            state = "q40"
             lenta[pos] = "0"
             pos -= 1
         if state == "q40" and lenta[pos] == " ":
             state = "q60"
             lenta[pos] = "1"
         if state == "q50" and lenta[pos] == "0":
-            state = "q50"
             lenta[pos] = "2"
             pos -= 1
         if state == "q50" and lenta[pos] == "1":
@@ -168,18 +100,13 @@ def turing(data):
             state = "q60"
             lenta[pos] = "1"
         if state == "q51" and lenta[pos] == "0":
-            state = "q51"
-            lenta[pos] = "0"
             pos -= 1
-        if state == "q51" and lenta[pos] == "1":
+
+        if state == "q51" and lenta[pos] in ["1", "2"]:
             state = "q60"
-            lenta[pos] = "1"
-        if state == "q51" and lenta[pos] == "2":
-            state = "q60"
-            lenta[pos] = "2"
+
         if state == "q51" and lenta[pos] == " ":
             state = "q52"
-            lenta[pos] = " "
             pos += 1
         if state == "q52" and lenta[pos] == "0":
             state = "q60"
