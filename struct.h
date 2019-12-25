3b5d2d3860f7fe4d91dc67f000699d79c5d9dcfc
Your Name <you@example.com>
2019-12-25 22:00:40 +0300

    new

---
+++ struct.h
@@ -0,0 +1,21 @@
+
+struct rade
+{
+	int v1;
+	int v2;
+	float v3;
+};
+ 
+struct str
+{
+	char name[20];    
+	long double time;         
+	char obs[20];
+	struct rade ra;
+	struct rade de;
+	int cf;
+	char region[30];
+	char af
+	char ff
+	int id 
+}; 
