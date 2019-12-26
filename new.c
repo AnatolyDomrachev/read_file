e0312db514a5252b185f5f839b0cdb4ff31a42f1
Ваше Имя <you@example.com>
2019-12-26 14:54:42 +0300

    333

--- new.c
+++ new.c
@@ -5,10 +5,12 @@ int read_str(FILE* fin)
 {
 	struct str sst;
 	char c;
-	fscanf(fin, "%c%s%d%c%Lf%c%30c%c%d%d%f%c%d%d%f%c%d%c%20c%c", &c, sst.name_s, &sst.name_n, &c, &sst.time, &c, sst.obs, &c, &sst.ra.v1, &sst.ra.v2, &sst.ra.v3, &c, &sst.de.v1, &sst.de.v2, &sst.de.v3, &c, &sst.cf, &c, sst.region, &c);
+	fscanf(fin, "%c%s%d%c%Lf%c%30c%c%d%d%f%c%d%d%f%c%d%c%20c%c%14c%c%9c%c%d%c", &c, sst.name_s, &sst.name_n, &c, &sst.time, &c, sst.obs, &c, &sst.ra.v1, &sst.ra.v2, &sst.ra.v3, &c, &sst.de.v1, &sst.de.v2, &sst.de.v3, &c, &sst.cf, &c, sst.region, &c, sst.af, &c, sst.ff, &c, &sst.id, &c);
 	sst.obs[30] = '\0';
 	sst.region[20] = '\0';
-	printf("%c %s %d %c %.15Lf %c %s %c %2d %2d %2.2f %c %2d %2d %2.2f %c %d  %c %s %c \n", c, sst.name_s, sst.name_n, c, sst.time, c, sst.obs , c, sst.ra.v1, sst.ra.v2, sst.ra.v3, c, sst.de.v1, sst.de.v2, sst.de.v3, c, sst.cf, c, sst.region, c);
+	sst.af[14] = '\0';
+	sst.ff[9] = '\0';
+	printf("%c %s %d %c %.15Lf %c %s %c %2d %2d %2.2f %c %2d %2d %2.2f %c %d  %c %s %c %s %c %s %c %d %c \n", c, sst.name_s, sst.name_n, c, sst.time, c, sst.obs , c, sst.ra.v1, sst.ra.v2, sst.ra.v3, c, sst.de.v1, sst.de.v2, sst.de.v3, c, sst.cf, c, sst.region, c, sst.af, c, sst.ff, c, sst.id, c);
 	return 1;
 }
 
