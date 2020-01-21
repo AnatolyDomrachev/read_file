10a1a2b5ad56ac798f339d793dfa9fc1f5e56752
Ваше Имя <you@example.com>
2020-01-21 19:52:47 +0300

    1

--- result/new.cpp
+++ result/new.cpp
@@ -120,12 +120,47 @@ char print_str(ofstream&  outfile, struct str sst)
 	outfile << endl;
 }
 
+// сложить числа из столбца TIME, например 2 и 10 число
+double sum_2_10(char* fname)
+{
+	struct str sst;
+	string s;
+	char c;
+	ifstream  infile  (fname);
+
+	for(int i=0; i<5; i++)
+		getline(infile, s);
+
+	long double sum = 0;         
+
+	for(int i=1; i<11;i++)
+	{
+		infile >> c;
+		read_str(infile, &sst);
+		if(i==2 || i==10)
+			sum += sst.time;
+	}
+
+	infile.close();
+	return sum;
+}
+
+
+
+
 int main()
 {
 	struct str sst;
 	char c;
 	string s;
-	long double sum = 0;         
+
+	// сложить числа из столбца TIME, например 2 и 10 число
+	long double sum;         
+	sum = sum_2_10("task1.txt");
+	cout.precision(15);
+	cout << "Сумма 2 и 10 = " << sum << endl;
+	// Конец
+
 
 	ifstream  infile  ("task1.txt");
 	ofstream  outfile  ("result.txt");
@@ -145,7 +180,6 @@ int main()
 		{
 			read_str(infile, &sst);
 			print_str(outfile, sst);
-			sum += sst.time - (int)sst.time;
 		}
 		else
 		{
@@ -155,8 +189,6 @@ int main()
 	}
 
 	outfile << c << s << endl;
-	cout.precision(20);
-	cout << sum << endl;
 	infile.close();
 	outfile.close();
 	return 0;
